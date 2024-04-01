137329ce6db684ea1bb4d247f895dde538621945
Rafael Moret <rafaelmoretgalan@gmail.com>
2024-04-01 14:00:19 +0200

    feat(modal): add new shortcut modal button animation to show message when hovered

--- src/components/AddShortcutModal/AddShortcutModalButton.vue
+++ src/components/AddShortcutModal/AddShortcutModalButton.vue
@@ -10,9 +10,15 @@ const openModal = () => emit('open-modal')
 
 <template>
   <button
-    class="mx-auto mt-auto mb-5 py-1 px-5 rounded-lg w-fit bg-gray-950 text-gray-300 hover:text-white"
+    class="group mx-auto mb-5 mt-auto w-fit overflow-hidden rounded-md border-2 border-gray-700 bg-gray-950 px-3.5 py-0.5 text-center text-lg font-semibold text-gray-300 shadow-md"
     @click="openModal"
   >
-    <Plus class="w-8 h-8" />
+    <div class="relative flex items-center transition-all duration-300 group-hover:mr-[7.5rem]">
+      <Plus class="h-8 w-8" />
+      <span
+        class="absolute left-12 block w-28 overflow-hidden text-ellipsis whitespace-nowrap text-left opacity-0 transition-all duration-300 group-hover:left-9 group-hover:opacity-100"
+        >Add shortcut</span
+      >
+    </div>
   </button>
 </template>
